c45a15c7194ba0a3924a79f836f37683aa0087ee
Jonatan Sivori <jonatansivori@gmail.com>
2016-02-11 09:39:58 -0300

    probando openshift

--- controller/index.php
+++ controller/index.php
@@ -1,6 +1,9 @@
 <?php
 /*error_reporting(E_ALL); 
-ini_set("display_errors", 1); */
+ini_set("display_errors", 1); 
+
+9rHdnG5F~!R&&+&yf8
+*/
 session_start();
 
 if((!isset($_POST['opt'])) or (!is_numeric($_POST['opt']))){
@@ -29,7 +32,8 @@ switch ($_POST['opt']){
 		break;
 		
 	case 2:
-		session_destroy();
+		if(isset($_SESSION) or (session_id() != ''))
+			session_destroy();
 		$pag = '../view/index.html';
 		break;
 		
--- model/db.php
+++ model/db.php
@@ -7,11 +7,19 @@ error_reporting(-1);
 ini_set ("date.timezone", "America/Argentina/Buenos_Aires");
 set_time_limit(0);
 
+
+/*
+Root User: adminjy8YLxm
+   Root Password: 18vyBUDUvt-j
+   Database Name: essence
+
+Connection URL: mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/
+*/
 function conectar(){
 	$dbname='essence';
-	$dbhost='localhost';
-    $dbuser='root';
-    $dbpass='';
+	$dbhost='//'.$OPENSHIFT_MYSQL_DB_HOST.':'.$OPENSHIFT_MYSQL_DB_PORT.'/';
+    $dbuser='adminjy8YLxm';
+    $dbpass='18vyBUDUvt-j';
 	
 	try{
 		$conn = new PDO(sprintf('mysql:host=%s;dbname=%s', $dbhost, $dbname), $dbuser, $dbpass);
